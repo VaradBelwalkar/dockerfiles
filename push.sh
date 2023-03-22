9fc0546f5063adc706a033cb5110fac74620da9c
VaradBelwalkar <belwalkarvarad@gmail.com>
2023-03-22 15:30:33 +0530

    some changes

--- push.sh
+++ push.sh
@@ -6,10 +6,10 @@ docker push varadbelwalkar/java
 docker push varadbelwalkar/nodejs
 docker push varadbelwalkar/python
 docker push varadbelwalkar/root_key_ssh_able_ubuntu
-docker push varadbelwalkar/root_key_ssh_able_arch
+#docker push varadbelwalkar/root_key_ssh_able_arch
 docker push varadbelwalkar/root_pass_ssh_able_ubuntu
-docker push varadbelwalkar/root_pass_ssh_able_arch
+#docker push varadbelwalkar/root_pass_ssh_able_arch
 docker push varadbelwalkar/user_key_ssh_able_ubuntu
-docker push varadbelwalkar/user_key_ssh_able_arch
+#docker push varadbelwalkar/user_key_ssh_able_arch
 docker push varadbelwalkar/user_pass_ssh_able_ubuntu
-docker push varadbelwalkar/user_pass_ssh_able_arch
+#docker push varadbelwalkar/user_pass_ssh_able_arch
