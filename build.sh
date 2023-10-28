fa75ac6cb5c01c2c92ef130ca4d88feff0d9c8f8
VaradBelwalkar <belwalkarvarad@gmail.com>
2023-10-28 12:25:10 +0530

    major changes

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker build -t varadbelwalkar/cpp -f cpp/dockerfile ./cpp
-docker build -t varadbelwalkar/docker -f docker/dockerfile ./docker
-docker build -t varadbelwalkar/go -f go/dockerfile ./go
+docker build -t varadbelwalkar/docker -f docker/dockerfile ./nested-docker
+docker build -t varadbelwalkar/go -f go/dockerfile ./golang
 docker build -t varadbelwalkar/java -f java/dockerfile ./java
 docker build -t varadbelwalkar/nodejs -f nodejs/dockerfile ./nodejs
 docker build -t varadbelwalkar/python -f python/dockerfile ./python
@@ -10,7 +10,7 @@ docker build -t varadbelwalkar/root_key_ssh_able_ubuntu -f root_key_ssh_able/ubu
 docker build -t varadbelwalkar/root_key_ssh_able_arch -f root_key_ssh_able/arch_dockerfile ./root_key_ssh_able
 docker build -t varadbelwalkar/root_pass_ssh_able_ubuntu -f root_pass_ssh_able/ubuntu_dockerfile ./root_pass_ssh_able
 docker build -t varadbelwalkar/root_pass_ssh_able_arch -f root_pass_ssh_able/arch_dockerfile ./root_pass_ssh_able
-docker build -t varadbelwalkar/user_key_ssh_able_ubuntu -f user_key_ssh_able/ubuntu_dockerfile ./user_key_ssh_able
-docker build -t varadbelwalkar/user_key_ssh_able_arch -f user_key_ssh_able/arch_dockerfile ./user_key_ssh_able
-docker build -t varadbelwalkar/user_pass_ssh_able_ubuntu -f user_pass_ssh_able/ubuntu_dockerfile ./user_pass_ssh_able
-docker build -t varadbelwalkar/user_pass_ssh_able_arch -f user_pass_ssh_able/arch_dockerfile ./user_pass_ssh_able
+docker build -t varadbelwalkar/user_key_ssh_able_ubuntu -f user_key_ssh_able/dockerfile ./user_key_ssh_able
+#docker build -t varadbelwalkar/user_key_ssh_able_arch -f user_key_ssh_able/arch_dockerfile ./user_key_ssh_able
+#docker build -t varadbelwalkar/user_pass_ssh_able_ubuntu -f user_pass_ssh_able/ubuntu_dockerfile ./user_pass_ssh_able
+#docker build -t varadbelwalkar/user_pass_ssh_able_arch -f user_pass_ssh_able/arch_dockerfile ./user_pass_ssh_able
--- push.sh
+++ push.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 docker push varadbelwalkar/cpp
-docker push varadbelwalkar/docker
+docker push varadbelwalkar/nested-docker
 docker push varadbelwalkar/go
 docker push varadbelwalkar/java
 docker push varadbelwalkar/nodejs
 docker push varadbelwalkar/python
-docker push varadbelwalkar/root_key_ssh_able_ubuntu
+#docker push varadbelwalkar/root_key_ssh_able_ubuntu
 #docker push varadbelwalkar/root_key_ssh_able_arch
-docker push varadbelwalkar/root_pass_ssh_able_ubuntu
+#docker push varadbelwalkar/root_pass_ssh_able_ubuntu
 #docker push varadbelwalkar/root_pass_ssh_able_arch
-docker push varadbelwalkar/user_key_ssh_able_ubuntu
+#docker push varadbelwalkar/user_key_ssh_able_ubuntu
 #docker push varadbelwalkar/user_key_ssh_able_arch
-docker push varadbelwalkar/user_pass_ssh_able_ubuntu
+#docker push varadbelwalkar/user_pass_ssh_able_ubuntu
 #docker push varadbelwalkar/user_pass_ssh_able_arch
